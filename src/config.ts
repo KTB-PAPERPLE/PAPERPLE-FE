a9ce2a32c55414c6124b8418872e7145cb5641f2
cjm0611 <cjm-0611@naver.com>
2024-12-17 01:45:00 +0900

    chore: API base 경로 수정

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
-export const API_BASE_URL = 'https://paperple.n-e.kr';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+export const AI_API_BASE_URL = process.env.REACT_AI_API_BASE_URL;
 export const STOCK_API_SECRET_KEY = process.env.REACT_APP_STOCK_API_SECRET_KEY;
